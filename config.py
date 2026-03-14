8d3ba8fa1a414468e0f5d59c5d6993a7f41f4e2d
Nour Mlaiki <nmlaiki2004@gmail.com>
2026-03-14 04:45:48 -0400

    added watson x

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
+import os 
 
 class Settings(BaseSettings):
     elevenlabs_api_key: str
     elevenlabs_stt_model_id: str = "eleven_monolingual_v1"
     elevenlabs_tts_model_id: str = "eleven_monolingual_v1"
-
+    ibm_watsonx_model_id: str = os.getenv("IBM_WATSONX_MODEL_ID")
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
