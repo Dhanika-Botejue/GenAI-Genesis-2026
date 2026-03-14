ee213611f257d5002ecf2504042833d508580ea9
Nour Mlaiki <nmlaiki2004@gmail.com>
2026-03-14 04:48:12 -0400

    added watson x

--- config.py
+++ config.py
@@ -5,6 +5,9 @@ class Settings(BaseSettings):
     elevenlabs_api_key: str
     elevenlabs_stt_model_id: str = "eleven_monolingual_v1"
     elevenlabs_tts_model_id: str = "eleven_monolingual_v1"
+    ibm_watsonx_api_key: str
+    ibm_watsonx_url: str
+    ibm_watsonx_project_id: str
     ibm_watsonx_model_id: str = os.getenv("IBM_WATSONX_MODEL_ID")
     model_config = SettingsConfigDict(
         env_file=".env",
